bb7db617905be1a3757fe19d88cb95dbc7ff4679
Proudust <proudust@gmail.com>
2019-06-25 09:46:47 +0900

    :art: fix by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,19 @@ module.exports = {
   devtool: false,
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: 'ts-loader'
-      }
-    ]
+        use: 'ts-loader',
+      },
+    ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
-    modules: ['node_modules']
+    modules: ['node_modules'],
   },
-  plugins: [new GasPlugin()]
+  plugins: [new GasPlugin()],
 };
